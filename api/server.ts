8066fb62f3eaeeb179effaf55c184f7cf4650340
an ha <an.ha@covergo.com>
2023-01-05 20:08:01 +0700

    refactor: add log to server.js

--- api/server.ts
+++ api/server.ts
@@ -11,7 +11,7 @@ const handler: Handler = async (
     return {
       statusCode: 500,
       headers: { "content-type": "application/json" },
-      body: JSON.stringify(`Missing ABLY_API_KEY environment variable.
+      body: JSON.stringify(`Missing ABLY_API_KEY (recevied: ${process.env.ABLY_API_KEY}) environment variable.
         If you're running locally, please ensure you have a ./.env file with a value for ABLY_API_KEY=your-key.
         If you're running in Netlify, make sure you've configured env variable ABLY_API_KEY.
         Please see README.md for more details on configuring your Ably API Key.`),
